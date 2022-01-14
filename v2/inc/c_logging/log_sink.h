ff56cdee7bbff25ac5692698d4975f944aaa8280
Dan Cristoloveanu <dcristo@microsoft.com>
2022-01-13 22:18:52 -0800

    More play

---
+++ v2/inc/c_logging/log_sink.h
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef LOG_SINK_H
+#define LOG_SINK_H
+
+#include "macro_utils/macro_utils.h"
+
+typedef void (*LOG_SINK_LOG_FUNC)(LOG_LEVEL log_level, char* message, const char* file, const char* func, int line);
+
+typedef struct LOG_SINK_TAG
+{
+    LOG_SINK_LOG_FUNC log_sink_log;
+} LOG_SINK;
+
+#endif /* LOG_SINK_H */
---
+++ v2/src/log_sink_etw.c
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "macro_utils/macro_utils.h"
+
+#include "c_logging/logger.h"
+#include "c_logging/log_sink_console.h"
+
+MU_DEFINE_ENUM_STRINGS_WITHOUT_INVALID(LOG_LEVEL, LOG_LEVEL_VALUES);
+
+void log_sink_console_log(LOG_LEVEL log_level, const char* message, const char* file, const char* func, int line)
+{
+    time_t t = time(NULL);
+
+    printf("%s Time:%.24s File:%s Func:%s Line:%d %s\r\n", MU_ENUM_TO_STRING(LOG_LEVEL, log_level), ctime(&t), file, func, line, message);
+}
+
+LOG_SINK console_log_sink = { log_sink_console_log };
